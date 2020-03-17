507d46a4f3739afbc28ff746ac42802853fecd80
Guilherme Soares Lima <guilherme.soares@icarros.com.br>
2020-03-17 20:53:25 -0300

    Pesquisa de imagens concluido, pendente a ajustes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import $ from "jquery";
 import axios from "axios";
 // import "../src/assets/search3.png";
 
+function exchangeSpacePlus(text) {
+  const isPhrase = text.split(" ").length > 0 ? true : false;
+  const phraseFormated = isPhrase ? text.replace(" ", "+") : text;
+  return phraseFormated;
+}
+
 function Header(props) {
   return (
     <div className="header">
@@ -16,22 +22,17 @@ function Header(props) {
           type="text"
           className="txtBusca"
           placeholder="Buscar..."
-          onChange={e => {
-            console.log(e.target.value, "valor");
-            console.log(props, "props");
-            // console.log(event);
-            // props.handleInputChange(e.target.value);
-          }}
+          value={props.phrase}
+          onChange={event => props.setStateDoPapaizineo(event.target.value)}
         />
-        {/* <button className="btnBusca" onClick={props.handleInputChange}> */}
         <button
           className="btnBusca"
-          // onClick={e => {
-          //   // console.log(e, "teste");
-          //   const b = props;
-          //   console.log("b", props);
-          // }}
-          onClick={props.onClick}
+          onClick={event => {
+            console.log(props.phrase);
+            const isPhrase = exchangeSpacePlus(props.phrase);
+            console.log("isPhrase", isPhrase);
+            props.setStateDoPapaizineo(isPhrase);
+          }}
         >
           Buscar
         </button>
@@ -64,12 +65,36 @@ function Header(props) {
   );
 }
 
+// function ListImages(props) {
+//   const listImg = [0, 1, 3, 4, 5, 6, 7, 8, 9];
+//   return (
+//     <div className="list">
+//       {listImg.map(value => (
+//         <ImageContainer key={value.toString()} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// function ImageContainer(props) {
+//   return (
+//     <div className="image-container">
+//       <div className="img-item">
+//         <img src="https://cdn.pixabay.com/user/2014/07/12/21-19-34-426_250x250.jpg" />
+//         {/* <img src="https://cdn.pixabay.com/user/2019/12/22/16-48-03-254_250x250.jpg" /> */}
+//       </div>
+//       <div className="image-detail">
+//         <div className="user-image"></div>
+//       </div>
+//     </div>
+//   );
+// }
+
 function ListImages(props) {
-  const listImg = [0, 1, 3, 4, 5, 6, 7, 8, 9];
   return (
     <div className="list">
-      {listImg.map(value => (
-        <ImageContainer key={value.toString()} />
+      {props.itens.map(value => (
+        <ImageContainer key={value.id.toString()} url={value.userImageURL} />
       ))}
     </div>
   );
@@ -79,7 +104,7 @@ function ImageContainer(props) {
   return (
     <div className="image-container">
       <div className="img-item">
-        <img src="https://cdn.pixabay.com/user/2014/07/12/21-19-34-426_250x250.jpg" />
+        <img src={props.url} />
         {/* <img src="https://cdn.pixabay.com/user/2019/12/22/16-48-03-254_250x250.jpg" /> */}
       </div>
       <div className="image-detail">
@@ -92,14 +117,17 @@ function ImageContainer(props) {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { key: "15592454-3d4064f5f4cbfd171f337e41e", phrase: "" };
+    this.state = {
+      itens: [],
+      key: "15592454-3d4064f5f4cbfd171f337e41e",
+      phrase: ""
+    };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.getValue = this.getValue.bind(this);
   }
 
   handleChange(e) {
-    // this.props.onPhraseChange(e.target.value);
-    this.setState({ phrase: e.target.value });
-    console.log("phrase", this.state.phrase);
+    console.log("phrase", e.target.value);
   }
 
   componentDidMount() {
@@ -127,10 +155,12 @@ class App extends Component {
       .get(
         // "https://pixabay.com/api/?key=" +
         //   this.state.key +
-        //   "&q=yellow+flowers&image_type=photo"
+        //   "&q=black+cat&image_type=photo"
         "https://pixabay.com/api/?key=" +
           this.state.key +
-          "&q=black+cat&image_type=photo"
+          "&q=" +
+          this.state.phrase +
+          "&image_type=photo"
       )
       .then(function(response) {
         // handle success
@@ -144,40 +174,69 @@ class App extends Component {
     //   // always executed
     // });
   }
-  // return (
-  //   <div className="main-screen">
-  //     <div className="main-header">
-  //       <Header />
-  //     </div>
-  //     <div className="main-body"></div>
-  //   </div>
-  // );
 
   handleInputChange(event) {
     const target = event.target;
     console.log("evento filho", event.target);
-    // const value = target.name === "isGoing" ? target.checked : target.value;
-    // const name = target.name;
-
-    // this.setState({
-    //   [name]: value
-    // });
-    // this.setState({
-    //   phase: event.target
-    // });
-    // console.log(event);
-    // console.log("phrase", this.state.phrase);
   }
 
+  getValue(value) {
+    this.setState({ itens: value });
+  }
+
+  changeState = value => {
+    this.setState({ phrase: value }); //atualizo o meu state
+    console.log("estado", this.state.phrase);
+    let resp;
+    const x = axios
+      .get(
+        // "https://pixabay.com/api/?key=" +
+        //   this.state.key +
+        //   "&q=black+cat&image_type=photo"
+        "https://pixabay.com/api/?key=" +
+          this.state.key +
+          "&q=" +
+          this.state.phrase
+        // "&image_type=photo"
+      )
+      .then(function(response) {
+        // handle success
+        console.log(response);
+        resp = response;
+
+        return response;
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+
+        return error;
+      });
+
+    console.log("x", x);
+    console.log(
+      "x",
+      x.then(value => {
+        console.log("valor", value);
+        this.setState({ itens: value.data.hits });
+        console.log("itens", this.state.itens);
+      })
+    );
+  };
+  estado;
   render() {
     const phrase = this.state.phrase;
     return (
       <div className="main-screen">
         <div className="main-header">
-          <Header onClick={this.handleChange} />
+          <Header
+            valuePhrase={this.handleChange}
+            setStateDoPapaizineo={this.changeState}
+            phrase={this.state.phrase}
+          />
         </div>
         <div className="main-body">
-          <ListImages />
+          <ListImages itens={this.state.itens} />
         </div>
       </div>
     );
