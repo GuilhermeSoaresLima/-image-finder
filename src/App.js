6f11e93d346196db61a49fa4656e22198ade0f37
Guilherme Soares Lima <guilherme.soares@icarros.com.br>
2020-03-18 16:35:11 -0300

    Botao para carregar mais imagens

--- src/App.js
+++ src/App.js
@@ -114,23 +114,36 @@ function ImageContainer(props) {
   );
 }
 
+function SeeMore(props) {
+  return (
+    <div className="container-button">
+      <button className="btn-see-more" onClick={event => props.more()}>
+        Ver mais...
+      </button>
+    </div>
+  );
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      itensPage: 0,
       itens: [],
       key: "15592454-3d4064f5f4cbfd171f337e41e",
       phrase: ""
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.getValue = this.getValue.bind(this);
+    this.showMore = this.showMore.bind(this);
+    this.changeState = this.changeState.bind(this);
   }
 
   handleChange(e) {
     console.log("phrase", e.target.value);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     // chamado depois da primeira reenderização
     console.log("DidMount");
     console.log("chave");
@@ -175,6 +188,62 @@ class App extends Component {
     // });
   }
 
+  showMore = () => {
+    console.log("teste botao");
+    if (this.state.itens.length <= 20) {
+      console.log(this.state.itens.length);
+      this.setState({ itensPage: 40 });
+      console.log("itens page", this.state.itensPage);
+      // this.changeState();
+
+      // if (this.state.itensPage > 0) {
+      //   axios
+      //     .get(
+      //       "https://pixabay.com/api/?key=" +
+      //         this.state.key +
+      //         "&q=" +
+      //         this.state.phrase +
+      //         `&per_page=${this.state.itensPage}`
+      //     )
+      //     .then(function(response) {
+      //       // handle success
+      //       console.log(response);
+
+      //       return response;
+      //     })
+      //     .catch(function(error) {
+      //       // handle error
+      //       console.log(error);
+
+      //       return error;
+      //     });
+      // }
+    } else {
+      this.setState({ itensPage: this.state.itensPage + 20 });
+    }
+
+    // axios
+    //   .get(
+    //     "https://pixabay.com/api/?key=" +
+    //       this.state.key +
+    //       "&q=" +
+    //       this.state.phrase +
+    //       `&per_page=${this.state.itensPage}`
+    //   )
+    //   .then(function(response) {
+    //     // handle success
+    //     console.log(response);
+
+    //     return response;
+    //   })
+    //   .catch(function(error) {
+    //     // handle error
+    //     console.log(error);
+
+    //     return error;
+    //   });
+  };
+
   handleInputChange(event) {
     const target = event.target;
     console.log("evento filho", event.target);
@@ -185,58 +254,78 @@ class App extends Component {
   }
 
   changeState = value => {
+    let moreItens = `&per_page=${this.state.itensPage}`;
     this.setState({ phrase: value }); //atualizo o meu state
     console.log("estado", this.state.phrase);
-    let resp;
-    const x = axios
-      .get(
-        // "https://pixabay.com/api/?key=" +
-        //   this.state.key +
-        //   "&q=black+cat&image_type=photo"
-        "https://pixabay.com/api/?key=" +
-          this.state.key +
-          "&q=" +
-          this.state.phrase
-        // "&image_type=photo"
-      )
-      .then(function(response) {
-        // handle success
-        console.log(response);
-        resp = response;
+    console.log("itens", moreItens);
+    const x =
+      this.state.itensPage === 0
+        ? axios
+            .get(
+              "https://pixabay.com/api/?key=" +
+                this.state.key +
+                "&q=" +
+                this.state.phrase
+              // "&per_page=200"
+            )
+            .then(function(response) {
+              // handle success
+              console.log(response);
 
-        return response;
-      })
-      .catch(function(error) {
-        // handle error
-        console.log(error);
+              return response;
+            })
+            .catch(function(error) {
+              // handle error
+              console.log(error);
 
-        return error;
-      });
+              return error;
+            })
+        : axios
+            .get(
+              "https://pixabay.com/api/?key=" +
+                this.state.key +
+                "&q=" +
+                this.state.phrase +
+                moreItens
+            )
+            .then(function(response) {
+              // handle success
+              console.log(response);
 
-    console.log("x", x);
-    console.log(
-      "x",
-      x.then(value => {
-        console.log("valor", value);
-        this.setState({ itens: value.data.hits });
-        console.log("itens", this.state.itens);
-      })
-    );
+              return response;
+            })
+            .catch(function(error) {
+              // handle error
+              console.log(error);
+
+              return error;
+            });
+
+    x.then(value => {
+      console.log("valor", value);
+      this.setState({ itens: value.data.hits });
+      console.log("itens", this.state.itens);
+    });
   };
-  estado;
+
   render() {
     const phrase = this.state.phrase;
     return (
       <div className="main-screen">
         <div className="main-header">
           <Header
-            valuePhrase={this.handleChange}
+            // valuePhrase={this.handleChange}
             setStateDoPapaizineo={this.changeState}
             phrase={this.state.phrase}
           />
         </div>
         <div className="main-body">
           <ListImages itens={this.state.itens} />
+          {this.state.itens.length >= 20 ? (
+            <SeeMore more={this.showMore} />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     );
